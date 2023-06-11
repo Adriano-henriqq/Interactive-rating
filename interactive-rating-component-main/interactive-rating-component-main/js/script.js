fff2837db984e248f20fb6270b75a6c8c81ac598
Adriano-henriqq <112038636+Adriano-henriqq@users.noreply.github.com>
2023-06-11 14:30:23 -0300

    adicionei os arquivos do projeto

---
+++ interactive-rating-component-main/interactive-rating-component-main/js/script.js
@@ -0,0 +1,23 @@
+const controleClassificacao = document.querySelectorAll("[data-classificacao]");
+controleClassificacao.forEach(controle => {
+    controle.addEventListener("click", ()=>{
+       verificaSeBotaoFoiClicado(controle); 
+       enviaClassificacao();
+         
+    })
+    
+})
+
+
+function verificaSeBotaoFoiClicado(controle) {
+    let botao = document.querySelectorAll(".classificacao .container__classificacao-active");
+    botao.length != 0 ? botao[0].classList.remove("container__classificacao-active") : botao.length;
+    controle.classList.toggle("container__classificacao-active");
+}
+
+function enviaClassificacao(){
+    const botao = document.querySelector("[data-botao]");
+    botao.addEventListener("click", function(){
+         window.location.href = '../page/congratulations.html'
+    })
+}
