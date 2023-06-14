bf344e2356126cc02e50538ca12f3ae3d0775e54
Adriano-henriqq <112038636+Adriano-henriqq@users.noreply.github.com>
2023-06-13 21:30:15 -0300

    alterei as funcionalidades do codigo ao enviar os valores para outra pagina

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const controleClassificacao = document.querySelectorAll("[data-classificacao]");
-controleClassificacao.forEach(controle => {
-    controle.addEventListener("click", ()=>{
+var paragrafo = document.querySelector(".container__paragrafo--congratulations");
+controleClassificacao.forEach(controle  => {
+    controle.addEventListener("click", (evento)=>{
        verificaSeBotaoFoiClicado(controle); 
+       var alvo = evento.target.innerText;
+        localStorage.setItem('valorDoClique', (alvo))
        enviaClassificacao();
-         
     })
-    
 })
 
+
+var valorDoClique = parseInt(localStorage.getItem('valorDoClique'));
+
+if(paragrafo != null){
+    exibeParagrafo(valorDoClique)
+}
 
 function verificaSeBotaoFoiClicado(controle) {
     let botao = document.querySelectorAll(".classificacao .container__classificacao-active");
@@ -15,9 +22,14 @@ function verificaSeBotaoFoiClicado(controle) {
     controle.classList.toggle("container__classificacao-active");
 }
 
-function enviaClassificacao(){
+ function enviaClassificacao(alvo){
     const botao = document.querySelector("[data-botao]");
     botao.addEventListener("click", function(){
-         window.location.href = "./congratulations.html";
+       window.location.href = "./congratulations.html";
     })
+}
+
+function exibeParagrafo (alvo){
+   return paragrafo.innerText = `You selected ${alvo} out of 5`;
+    
 }
